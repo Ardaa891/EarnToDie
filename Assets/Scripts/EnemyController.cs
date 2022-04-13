0a84b77ad74f4b9eba89a977eef0b26099b2d1c8
Ardaa891 <ardaaydar@funikagames.com>
2022-04-13 18:01:14 +0300

    13.04.2022
    v0.2

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -9,6 +9,13 @@ public class EnemyController : MonoBehaviour
     Rigidbody rb;
     Animator anim;
 
+    public  int xForce, yForce, zForce;
+    public int forceAmount = 500;
+
+    public float power = 10.0f;
+    public float radius = 5.0f;
+    Vector3 explosionPos;
+
     private void Awake()
     {
         SetRagdollParts();
@@ -18,6 +25,11 @@ public class EnemyController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
+        xForce = Random.Range(1, 10);
+        yForce = Random.Range(20, 35);
+        zForce = Random.Range(15, 30);
+
+        explosionPos = transform.position;
     }
 
     void SetRagdollParts()
@@ -52,18 +64,54 @@ public class EnemyController : MonoBehaviour
         {
             c.isTrigger = false;
             c.GetComponent<Rigidbody>().useGravity = true;
+            c.GetComponent<Rigidbody>().AddForce(xForce, yForce, zForce, ForceMode.Impulse);
             //c.attachedRigidbody.velocity = Vector3.zero;
         }
+
+        Die();
+    }
+
+    void TurnOnRagdollv2()
+    {
+        gameObject.GetComponent<BoxCollider>().enabled = false;
+
+        rb.useGravity = false;
+
+        anim.enabled = false;
+        anim.avatar = null;
+
+        foreach (Collider c in RagdollParts)
+        {
+            c.isTrigger = false;
+            c.GetComponent<Rigidbody>().useGravity = true;
+            c.GetComponent<Rigidbody>().AddForce(1, 1, 2, ForceMode.Impulse);
+            //c.attachedRigidbody.velocity = Vector3.zero;
+        }
+
+        Die();
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") )
         {
             TurnOnRagdoll();
             
 
         }
+        if(collision.gameObject.CompareTag("Enemy"))
+        {
+            TurnOnRagdollv2();
+            
+        }
+
+        
+    }
+
+    IEnumerator Die()
+    {
+        yield return new WaitForSecondsRealtime(5f);
+        gameObject.SetActive(false);
     }
 
 }
