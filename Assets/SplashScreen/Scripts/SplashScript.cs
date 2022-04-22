088ea22e7002781b2c15a6a2d1b994a6d8b4571a
Ardaa891 <ardaaydar@funikagames.com>
2022-04-22 15:57:07 +0300

    22.04.2022
    v2

---
+++ Assets/SplashScreen/Scripts/SplashScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScript : MonoBehaviour
+{
+    public string Scene_Name;
+    // In this example we show how to invoke a coroutine and
+    // continue executing the function in parallel.
+
+    private IEnumerator coroutine;
+
+    void Start()
+    {
+        coroutine = WaitAndPrint(3.0f);
+        StartCoroutine(coroutine);
+    }
+
+    // every 2 seconds perform the print()
+    private IEnumerator WaitAndPrint(float waitTime)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(waitTime);
+            SceneManager.LoadScene(Scene_Name);
+        }
+    }
+}
